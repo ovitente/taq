7008b5c325d43b5182fdf4e259d85e75c641bf4c
Ivan Kostrubin <7spawn@gmail.com>
2020-04-24 16:57:07 +0300

    adding config support

--- main.go
+++ main.go
@@ -2,41 +2,39 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"io/ioutil"
+	"log"
 
 	"gopkg.in/yaml.v2"
 )
 
-type Config struct {
+type config struct {
 	server_host    string `yaml:"server_host"`
-	query_port     int    `yaml:"query_port"`
-	sid            int    `yaml:"sid"`
+	query_port     int64  `yaml:"query_port"`
+	sid            int64  `yaml:"sid"`
 	query_login    string `yaml:"query_login"`
 	query_password string `yaml:"query_password"`
 }
 
-func ConfigInit(configPath string) (*Config, error) {
-	// Create config structure
-	config := &Config{}
-
-	// Open config file
-	file, err := os.Open(configPath)
+func (c *config) getConf() *config {
+	configPath := "./config.yml"
+	yamlFile, err := ioutil.ReadFile(configPath)
 	if err != nil {
-		return nil, err
+		log.Printf("Config file not found #%v ", err)
 	}
-	defer file.Close()
-
-	// Init new yaml decode
-	d := yaml.NewDecoder(file)
-
-	// Start yaml decodint from file
-	if err := d.Decode(&config); err != nil {
-		return nil, err
+	err = yaml.Unmarshal(yamlFile, c)
+	if err != nil {
+		log.Fatalf("Unmarshal: %v", err)
 	}
 
-	return config, nil
+	return c
+
 }
 
 func main() {
-	fmt.Println("vim-go")
+	var c config
+	c.getConf()
+
+	fmt.Println(c)
+
 }
